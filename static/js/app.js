d17178978be423716f0e35a1a9be31cfed8e77ff
Kevin Eggert <kevineggert@Kevins-MacBook-Air.local>
2020-08-23 17:03:01 -0500

    Progress

--- static/js/app.js
+++ static/js/app.js
@@ -9,6 +9,12 @@ function buildMetadata(sample) {
     // Using html to clear any existing metadata
     panel.html("")
 
+    // Use `Object.entries` to add each key and value pair to the panel
+    // Hint: Inside the loop, you will need to use d3 to append new tags for each key-value in the metadata.
+    Object.entries(data).forEach(([key, value]) => {
+        panel.append("h6").text(`${key}: ${value}`);
+    });
+
         var barData = {
             x: microbe.sample_values.slice(0, 10).reverse(),
             y: microbe.otu_ids,
@@ -26,8 +32,20 @@ function buildMetadata(sample) {
 });
 
 function init () {
+    // Reference dropdown select element
     var dropdownMenu = d3.select("#selDataset");
-    var dataset = dropdownMenu.
+    // Use the list of samples names to populate the different select options
+    d3.json("./data/samples.json").then((data) => {
+    
+    });
+
 }
 
+function changeOptions(newSample) {
+    // Fetch new data when a new sample is selected
+    buildCharts(newSample);
+    buildMetadata(newSample);
+}
+
+// Finish by initializaing the dashboard
 init();
