bbe10558abba032f730bf26d0f9a72460e808b34
Kevin Eggert <kevineggert@Kevins-MacBook-Air.local>
2020-08-23 16:44:47 -0500

    Panel panel panel

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,30 @@
 // Use D3 JSON to fetch data from JSON file
 // Incoming data is internally referred to as incomingData
+function buildMetadata(sample) {
+    d3.json("./data/samples.json").then((data) => {
+        console.log(data);
+    // Use D3 to select the panel with an id of sample-metadata
+    var panel = d3.select("#sample-metadata");
 
-d3.json("./data/samples.json").then((data) => {
-    console.log(data);
+        var barData = {
+            x: microbe.sample_values.slice(0, 10).reverse(),
+            y: microbe.otu_ids,
+            text: otu_labels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: "h"
+        }
 
-
-    var barData = {
-        x: microbe.sample_values.slice(0, 10).reverse(),
-        y: microbe.otu_ids,
-        text: otu_labels.slice(0, 10).reverse(),
-        type: "bar",
-        orientation: "h"
-    }
-
-    var barLayout = {
-        title: "Top 10 Bacteria Cultures",
-    }
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures",
+        }
 
 
     Plotly.newPlot("bar", barData, barLayout)
 });
+
+function init () {
+    var dropdownMenu = d3.select("#selDataset");
+    var dataset = dropdownMenu.
+}
+
+init();
