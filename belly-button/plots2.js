296c71fc8f804e64c59ff06656c2b988ee9f13c4
keggert <64288336+keggert@users.noreply.github.com>
2020-09-04 13:18:29 -0500

    Add files via upload

--- belly-button/plots2.js
+++ belly-button/plots2.js
@@ -1,15 +1,16 @@
-function buildMetadata(sample) {
+function filterMicrobes(sample) {
     d3.json("samples.json").then((data) => {
+        console.log(data);
         var metadata = data.metadata;
         // Filter our data to get the desired sample number for the object
         var resultsArray = metadata.filter(sampleObject => sampleObject.id == sample);
-        var result = resultsArray[0];
+        var microbe = resultsArray[0];
         // Use d3 to select the panel with an id of sample-metadata
         var panel = d3.select("#sample-metadata");
         // We need to use html("") to clear any existing data
         panel.html("");
         // We need to use object.entries to add each key and value pair to our panel
-        Object.entries(result).forEach(([key, value]) => {
+        Object.entries(microbe).forEach(([key, value]) => {
             panel.append("h6").text(`${key.toUpperCase()}: ${value}`);
         });
     });
@@ -19,15 +20,12 @@ function buildChart(sample) {
     d3.json("samples.json").then((data) => {
         var samples = data.samples;
         var resultsArray = samples.filter(sampleObject => sampleObject.id == sample);
-        var result = resultsArray[0];
+        var microbe = resultsArray[0];
 
-        var otu_ids = result.otu_ids;
-        var otu_labels = result.otu_labels;
-        var sample_values = result.sample_values;
 
         // Bubble Chart Time
         var bubbleLayout = {
-            title: "Cultures of Bacteria Per Sample",
+            title: "Top 10 Bacterial Cultures",
             margin: { t: 0},
             hovermode: "closest",
             xaxis: { title: "OTU ID"},
@@ -36,32 +34,32 @@ function buildChart(sample) {
 
         var bubbleData = [
             {
-                x: otu_ids,
-                y: sample_values,
-                text: otu_labels,
+                x: microbe.otu_ids,
+                y: microbe.sample_values,
+                text: microbe.otu_labels,
                 mode: "markers",
                 marker: {
-                    size: sample_values,
-                    color: otu_ids,
+                    size: microbe.sample_values,
+                    color: microbe.otu_ids,
                     colorscale: "Earth"
                 }
             }
         ];
         Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
-        var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+        var yticks = microbe.otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
         var barData = [
           {
             y: yticks,
-            x: sample_values.slice(0, 10).reverse(),
-            text: otu_labels.slice(0, 10).reverse(),
+            x: microbe.sample_values.slice(0, 10).reverse(),
+            text: microbe.otu_labels.slice(0, 10).reverse(),
             type: "bar",
             orientation: "h",
           }
         ];
     
         var barLayout = {
-          title: "Top 10 Bacteria Cultures Discovered",
+          title: "Top 10 Bacterial Cultures",
           margin: { t: 30, l: 150 }
         };
     
@@ -72,30 +70,30 @@ function buildChart(sample) {
 
 function init() {
     // Grab a reference to our dropdown select element
-    var selector = d3.select("#selDataset");
+    var dropdown = d3.select("#selDataset");
 
     // Use sample names list to populate select options
     d3.json("samples.json").then((data) => {
-        var sampleNames = data.names;
+        var sampleName = data.names;
 
-        sampleNames.forEach((sample) => {
-            selector
+        sampleName.forEach((sample) => {
+            dropdown
                 .append("option")
                 .text(sample)
                 .property("value", sample);
         });
 
         // Use the first sample from the list to build initial plots
-        var firstSample = sampleNames[0];
+        const firstSample = sampleName[0];
         buildChart(firstSample);
-        buildMetadata(firstSample);
+        filterMicrobes(firstSample);
     });
 }
 
 function changeOptions(newSample) {
     // Fetch new data everytime a new sample gets selected
     buildChart(newSample);
-    buildMetadata(newSample);
+    filterMicrobes(newSample);
 }
 
 // Initialize our dashboard
